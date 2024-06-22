68d94d90951d13a60fbc30b8c940b1fe411a9548
Lcc <youtao531@163.com>
2024-06-22 12:34:37 +0800

    :tada:	first commit

---
+++ src/main/java/com/framework/cloud/JetbrainsCloudApplication.java
@@ -0,0 +1,13 @@
+package com.framework.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JetbrainsCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JetbrainsCloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/framework/cloud/JetbrainsCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.framework.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JetbrainsCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

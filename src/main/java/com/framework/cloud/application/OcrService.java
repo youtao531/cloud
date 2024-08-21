b0c9d1902a0ec4093eb998ca69913742c558f105
Lcc <youtao531@163.com>
2024-08-21 09:43:14 +0800

    feat: add ocr service interface

---
+++ src/main/java/com/framework/cloud/application/OcrService.java
@@ -0,0 +1,25 @@
+package com.framework.cloud.application;
+
+import com.framework.cloud.domain.model.CardType;
+import com.framework.cloud.domain.model.CountryModel;
+import com.framework.cloud.interfaces.vo.OcrVO;
+import org.springframework.lang.Nullable;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Map;
+
+/**
+ * @author Lcc 2023/10/12 16:59
+ */
+public interface OcrService {
+
+    /**
+     * 百度飞桨OCR文字提取
+     *
+     * @param image        照片信息
+     * @param countryModel 国家信息
+     * @param cardType     卡片类型
+     * @param headers      请求标头
+     */
+    OcrVO ocr(MultipartFile image, CountryModel countryModel, @Nullable CardType cardType, Map<String, String> headers);
+}
